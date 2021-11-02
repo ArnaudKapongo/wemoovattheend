5ea218fe9a2ac5a676b9c9a76b81e746e5745211
Arnaud Kapongo <kapongoarnaud@gmail.com>
2021-11-02 18:32:10 +0100

    current user

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys/keys');
 const { check , validationResult } = require('express-validator');
 
 const User = require('../model/User/User');
@@ -71,7 +73,18 @@ router.post('/', [
 
 
     // Return jsonwebtoken
-    res.send('User registered');
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+
+    jwt.sign(payload, keys.secretOrKey,
+    { expiresIn: 360000 },
+    ( err, token) => {
+        if(err) throw err;
+        res.json({ token });
+    });
 
     } catch(err) {
     console.error(err.message);
