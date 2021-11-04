455b135993c884e4314852af577a0b9c2c5030aa
Arnaud Kapongo <kapongoarnaud@gmail.com>
2021-11-04 14:39:50 +0100

    Add Delete consultant routes

--- server/model/User/Profile.js
+++ server/model/User/Profile.js
@@ -5,110 +5,81 @@ const ProfileSchema = new Schema({
       user: {
           type: mongoose.Schema.Types.ObjectId,
           ref: 'user'
-      },
-      company: {
-        type: String
+          },
+      name: {
+          type: String
       },
       website: {
-        type: String
+          type: String 
       },
-      location: {
-        type: String
+      address: {
+          type: String
       },
-      status: {
-        type: String,
-        required: true
+      digit: {
+          type: Number
       },
-      skills: {
-        type: [String],
-        required: true
+      generalSkills: {
+          type: [String]
       },
-      bio: {
-        type: String
+      description: {
+          type: String
       },
-      githubusername: {
-        type: String
-      },
-      experience: [
+      consultant: [
         {
-          title: {
+        month: {
             type: String,
             required: true
           },
-          company: {
+          name: {
             type: String,
             required: true
           },
-          location: {
-            type: String
-          },
-          from: {
-            type: Date,
+          surname: {
+            type: String,
             required: true
           },
-          to: {
-            type: Date
+          skillstechnical: {
+            type: Number
           },
-          current: {
-            type: Boolean,
-            default: false
+          skillsfunctional: {
+            type: Number
           },
-          description: {
-            type: String
+          progress: {
+            type: Number
+          },
+          realisation: {
+            type: Number
+          },
+          transfert: {
+            type: Number
+          },
+          remark: {
+              type: String
+          },
+          note: {
+              type: String
+          },
+          rigor: {
+              type: Number
+          },
+          punctuality: {
+              type: Number
+          },
+          autonomy: {
+              type: Number
+          },
+          communication: {
+              type: Number
+          },
+          strength: {
+              type: Number
+          }, 
+          date: {
+              type: Date,
+              default: Date.now
           }
         }
-      ],
-      education: [
-        {
-          school: {
-            type: String,
-            required: true
-          },
-          degree: {
-            type: String,
-            required: true
-          },
-          fieldofstudy: {
-            type: String,
-            required: true
-          },
-          from: {
-            type: Date,
-            required: true
-          },
-          to: {
-            type: Date
-          },
-          current: {
-            type: Boolean,
-            default: false
-          },
-          description: {
-            type: String
-          }
-        }
-      ],
-      social: {
-        youtube: {
-          type: String
-        },
-        twitter: {
-          type: String
-        },
-        facebook: {
-          type: String
-        },
-        linkedin: {
-          type: String
-        },
-        instagram: {
-          type: String
-        }
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-});
+      ]
+    });
     
-module.exports = mongoose.model('profile', ProfileSchema);
+module.exports  = mongoose.model('profile', ProfileSchema);
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,13 +1,246 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
+const Profile = require('../model/User/Profile');
+const User = require('../model/User/User');
+
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+
+router.get('/me', auth, async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  POST api/profile
+// @desc   Create or update user profile
+// @access Private
+
+router.post('/', auth
+, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        name,
+        website,
+        address,
+        digit,
+        generalSkills,
+        description
+    } = req.body;
+
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (name) profileFields.name = name;
+    if (website) profileFields.website = website;
+    if (address) profileFields.address = address;
+    if (digit) profileFields.digit = digit;
+    if (description) profileFields.description;
+    if (generalSkills) {
+        profileFields.generalSkills = generalSkills.split(',').map(generalSkills => generalSkills.trim());
+    }
+
+    try {
+        let profile = Profile.findOne({ user: req.user.id });
+
+        if (profile) {
+            // Update 
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true, upsert: true, setDefaultsOnInsert: true });
+
+            return res.json(profile);
+
+        }
+
+        // Create
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+
+});
 
 // @route  GET api/profile
-// @desc   Test route
+// @desc   Get all profiles
 // @access Public
 
-router.get('/', (req, res) => {
-    res.send('Profile Routes');
+router.get('/', async (req, res) => {
+
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
+
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access Public
+
+router.get('/user/:user_id', async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  DELETE api/profile
+// @desc   Delete profile user & posts
+// @access Private
+
+router.delete('/', auth, async (req, res) => {
+
+    try {
+        await Profile.findOneAndRemove({ user: req.user.id });
+        
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted '});
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  PUT api/profile/consultant
+// @desc   Add profile consultant
+// @access Private
+
+router.put('/consultant', [auth, [
+
+    check('month', 'Month is required')
+    .not()
+    .isEmpty(),
+    check('name', 'Name is required')
+    .not()
+    .isEmpty(),
+    check('surname', 'Surname is required')
+    .not()
+    .isEmpty()
+
+] ],  async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { 
+        month,
+        name,
+        surname,
+        skillstechnical,
+        skillsfunctional,
+        progress,
+        realisation,
+        transfert,
+        remark,
+        note,
+        rigor,
+        punctuality,
+        autonomy,
+        communication,
+        strength
+    } = req.body;
+
+    const newConsultant = {
+        month,
+        name,
+        surname,
+        skillstechnical,
+        skillsfunctional,
+        progress,
+        realisation,
+        transfert,
+        remark,
+        note,
+        rigor,
+        punctuality,
+        autonomy,
+        communication,
+        strength
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.consultant.unshift(newConsultant);
+
+      await profile.save();
+
+      res.json(profile);
+        
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');  
+    }
+
+});
+
+// @route  PUT api/profile/consultant/:consultant_id
+// @desc   Delete consultant from profile
+// @access Private
+
+router.delete('/consultant/:consultant_id', auth, async (req, res) => {
+   
+   try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.consultant.map(item => item.id).indexOf(req.params.consultant_id);
+
+    profile.consultant.splice(removeIndex, 1);
+    
+    await profile.save();
+
+    res.json(profile);
+
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error');
+   }
+});
+
 
 module.exports = router;
